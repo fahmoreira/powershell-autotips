1ffa8a5d5dc1d27dd89f667a56d883145703feb7
Fábio Moreira Silva <moreira.sva@gmail.com>
2022-01-21 11:24:07 -0300

    Add files via upload
    Sometimes there will a lot of policies on the infrastructure that will block to run powershell scripts and for me I just put the "start" in a ".bat" file.

---
+++ #BackupUnidades.bat
@@ -0,0 +1,4 @@
+@echo off
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe  -noprofile -ExecutionPolicy Bypass -file "\\mz-vv-fs-029\publico$\FabioMS\FMS\Auto\GetNetworkDrivers.ps1"
+
+timeout /T 5
